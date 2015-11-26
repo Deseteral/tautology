7f19813286017f642411dee43f9748a998927d1f
Deseteral <deseteral@gmail.com>
2015-11-26 22:40:59 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('default', ['build-js', 'build-html'], () => {
+  return gulp;
+});
+
+gulp.task('build-js', () => {
+  return gulp
+    .src('src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('build-html', () => {
+  return gulp
+    .src('src/**/*.html')
+    .pipe(gulp.dest('build'));
+});
