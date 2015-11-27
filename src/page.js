8e67f1cb759c4aafd44932b61c7fd3a2bbdffb74
Deseteral <deseteral@gmail.com>
2015-11-28 00:36:03 +0100

    This actually works now

--- src/page.js
+++ src/page.js
@@ -1,43 +1,97 @@
 
-function toRPN(input) {
-  let queue = [];
+function convertToRPN(input) {
+  let queue = '';
   let stack = [];
 
-  while (input.length !== 0) {
-    let first = input[0];
+  input = input.replace(/\s+/g, '');
+  input = input.split(/([\&\|\=\>\(\)])/);
 
-    input = input.substr(1);
-
-    if (first === '0' || first === '1') {
-      // If the symbol is a value, add it to the queue
-      queue.push(first);
-    } else if (first === '&' || first === '|') {
-      // If the symbol is an operator
-
-      // Top of the stack
-      let o2 = stack[stack.length - 1];
-
-      while (stack.length !== 0 && o2 !== '(') {
-        queue.push(stack.pop());
-      }
-
-      stack.push(first);
-    } else if (first === '(') {
-      stack.push(first);
-    } else if (first === ')') {
-      let top = stack.pop();
-
-      while (top !== '(' && top !== undefined) {
-        queue.push(top);
-        top = stack.pop();
-      }
+  // Clean the array
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === '') {
+      input.splice(i, 1);
     }
   }
 
-  // Add the rest of the operators to the queue
-  while (stack.length !== 0) {
-    queue.push(stack.pop());
+  for (let i = 0; i < input.length; i++) {
+    let token = input[i];
+
+    if (token === '0' || token === '1') {
+      queue += token + ' ';
+    } else if ('&|>='.indexOf(token) !== -1) {
+      let o1 = token;
+      let o2 = stack[stack.length - 1];
+      while ('&|>='.indexOf(o2) !== -1) {
+        queue += stack.pop() + ' ';
+        o2 = stack[stack.length - 1];
+      }
+
+      stack.push(o1);
+    } else if (token === '(') {
+      stack.push(token);
+    } else if (token === ')') {
+      while (stack[stack.length - 1] !== '(') {
+        queue += stack.pop() + ' ';
+      }
+
+      stack.pop();
+    }
   }
 
-  return queue.join(' ');
+  while (stack.length > 0) {
+    queue += stack.pop() + ' ';
+  }
+
+  return queue;
+}
+
+function calculate(rpn) {
+  let stack = [];
+
+  while (rpn.length !== 0) {
+    let symbol = rpn[0];
+    rpn = rpn.substr(1);
+
+    let a;
+    let b;
+    let val;
+
+    // If symbol is a value
+    switch (symbol) {
+      case '0':
+        stack.push(0);
+        break;
+      case '1':
+        stack.push(1);
+        break;
+      case '&':
+        a = stack.pop();
+        b = stack.pop();
+        stack.push(b && a);
+        break;
+      case '|':
+        a = stack.pop();
+        b = stack.pop();
+        stack.push(b || a);
+        break;
+      case '=':
+        a = stack.pop();
+        b = stack.pop();
+        val = (b === a) ? 1 : 0;
+
+        stack.push(val);
+        break;
+      case '>':
+        a = stack.pop();
+        b = (!(stack.pop())) ? 1 : 0;
+        val = (b || a) ? 1 : 0;
+
+        stack.push(val);
+        break;
+      default:
+        break;
+    }
+  }
+
+  return parseInt(stack);
 }
