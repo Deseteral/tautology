bd0db4cbeff9ac815f4b9962f4417ea2dfcdb717
Adam Golab <adam@brainhub.pl>
2018-10-01 19:26:07 +0200

    installed and configured eslint, fixed all errors

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,21 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', ['build-js', 'build-html', 'build-css'], () => {
-  return gulp;
-});
+gulp.task('default', ['build-js', 'build-html', 'build-css'], () => gulp);
 
-gulp.task('build-js', () => {
-  return gulp
-    .src('src/**/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('build'));
-});
+gulp.task('build-js', () => gulp
+  .src('src/**/*.js')
+  .pipe(sourcemaps.init())
+  .pipe(babel({
+    presets: ['es2015'],
+  }))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('build')));
 
-gulp.task('build-html', () => {
-  return gulp
-    .src('src/**/*.html')
-    .pipe(gulp.dest('build'));
-});
+gulp.task('build-html', () => gulp
+  .src('src/**/*.html')
+  .pipe(gulp.dest('build')));
 
-gulp.task('build-css', () => {
-  return gulp
-    .src('src/**/*.css')
-    .pipe(gulp.dest('build'));
-});
+gulp.task('build-css', () => gulp
+  .src('src/**/*.css')
+  .pipe(gulp.dest('build')));
