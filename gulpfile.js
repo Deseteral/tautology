5afdd834b7c904d9dde7fe3546cfdb28e5240573
Joan <joan.gawelda@gmail.com>
2018-10-16 12:43:06 +0300

    Setup builds for production and development

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,14 @@
 const gulp = require('gulp');
-const babelify = require('babelify');
 const sourcemaps = require('gulp-sourcemaps');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
+const watchify = require('watchify');
+const uglify = require('gulp-uglify');
 
-gulp.task('default', ['move-translations', 'browserify', 'build-html', 'build-css'], () => gulp);
+gulp.task('default', ['build-html', 'build-css', 'build-prod'], () => gulp);
+gulp.task('watch', ['build-html', 'build-css'], () => watch(),);
 
-gulp.task('move-translations', () => gulp
-  .src(['./i18next/*.json'])
-  .pipe(gulp.dest('build/i18next')));
-  
-gulp.task('browserify', (cb) => {
-  const bundler = browserify({
-    entries: [__dirname + '/src/page.js'],
-    basedir: __dirname,
-    globals: false,
-    debug: true // enables source maps
-  })
-  bundler
-    .transform(babelify.configure({
-      presets: ["es2015"]
-    }))
-    .require(__dirname + '/src/page.js')
-    .bundle()
-    .on('error', cb)
-    .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('build'))
-    .on('end', cb)
-})
-  
 gulp.task('build-html', () => gulp
 .src('src/**/*.html')
 .pipe(gulp.dest('build')));
@@ -40,3 +16,51 @@ gulp.task('build-html', () => gulp
 gulp.task('build-css', () => gulp
 .src('src/**/*.css')
 .pipe(gulp.dest('build')));
+
+gulp.task('build-prod', () => {
+  let bundler = browserify({
+    entries: [__dirname + '/src/page.js'],
+    basedir: __dirname,
+    globals: false,
+  }).transform("babelify", { presets: ["@babel/preset-env"] });
+
+  return bundler
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('build'));
+})
+
+function compile(watch) {
+  let bundler = watchify(browserify({
+    entries: [__dirname + '/src/page.js'],
+    basedir: __dirname,
+    globals: false,
+    debug: true // enables source maps
+  }).transform("babelify", { presets: ["@babel/preset-env"] }));
+  
+  function rebundle() {
+    bundler
+    .require(__dirname + '/src/page.js')
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build'));
+  }
+  
+  if (watch) {
+    bundler.on('update', () => {
+      console.log('Bundling...');
+      rebundle();
+    });
+  }
+  
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
+}
